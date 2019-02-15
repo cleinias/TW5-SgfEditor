3aee535679bf3fe2c46aacce5ca05bc86e837e26
Stefano Franchi <stefano.franchi@gmail.com>
2019-02-15 13:19:10 -0600

    Added new tiddler type text/vnd-tiddlywiki-sgf

---
+++ typed-parser.js
@@ -0,0 +1,27 @@
+/*\
+title: $:/plugins/cleinias/sgfeditor/typed-parser.js
+type: application/javascript
+module-type: parser
+
+This parser wraps sgf go game records into an sgfeditor
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var SGFParser = function(type,text,options) {
+	var element = {
+			type: "sgfeditor",
+			tag: "$sgfeditor",
+			text: text
+		};
+	this.tree = [element];
+console.log(text);
+};
+
+exports["text/vnd-tiddlywiki-sgf"] = SGFParser;
+
+})();
